a1474ecaeb4b5946202915f922e90f9a1e763e29
kotabrog <suzuki.kota0331@gmail.com>
2024-04-25 23:38:27 +0900

    clear chapter11

---
+++ tailr/src/lib.rs
@@ -0,0 +1,286 @@
+use crate::TakeValue::*;
+use clap::Parser;
+use std::{
+    error::Error,
+    io::{BufRead, Read, Seek, BufReader, SeekFrom},
+    fs::File,
+};
+use regex::Regex;
+use once_cell::sync::OnceCell;
+
+static NUM_RE: OnceCell<Regex> = OnceCell::new();
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug, PartialEq)]
+enum TakeValue {
+    PlusZero,
+    TakeNum(i64),
+}
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: TakeValue,
+    bytes: Option<TakeValue>,
+    quiet: bool,
+}
+
+#[derive(Debug, Parser)]
+pub struct ConfigArgs {
+    #[arg(name = "FILE", required = true, help = "Input file(s)")]
+    files: Vec<String>,
+    #[arg(short = 'n', long, default_value = "10", conflicts_with = "bytes", help = "Number of lines")]
+    lines: String,
+    #[arg(short = 'c', long, help = "Number of bytes")]
+    bytes: Option<String>,
+    #[arg(short, long, help = "Suppress headers")]
+    quiet: bool,
+}
+
+fn parse_num(val: &str) -> MyResult<TakeValue> {
+    let num_re =
+        NUM_RE.get_or_init(|| Regex::new(r"^([+-])?(\d+)$").unwrap());
+
+    match num_re.captures(val) {
+        Some(caps) => {
+            let sign = caps.get(1).map_or("-", |m| m.as_str());
+            let num = format!("{}{}", sign, caps.get(2).unwrap().as_str());
+            if let Ok(val) = num.parse() {
+                if sign == "+" && val == 0 {
+                    Ok(PlusZero)
+                } else {
+                    Ok(TakeNum(val))
+                }
+            } else {
+                Err(val.into())
+            }
+        }
+        None => Err(val.into()),
+    }
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let args = ConfigArgs::parse();
+    let lines = parse_num(&args.lines)?;
+    let bytes = args.bytes.as_ref().map(|b| parse_num(b)).transpose()?;
+    Ok(Config {
+        files: args.files,
+        lines,
+        bytes,
+        quiet: args.quiet,
+    })
+}
+
+fn count_lines_bytes(filename: &str) -> MyResult<(i64, i64)> {
+    let mut file = BufReader::new(File::open(filename)?);
+    let mut num_lines = 0;
+    let mut num_bytes = 0;
+    let mut buf = vec![];
+    loop {
+        let bytes_read = file.read_until(b'\n', &mut buf)?;
+        if bytes_read == 0 {
+            break;
+        }
+        num_lines += 1;
+        num_bytes += bytes_read as i64;
+        buf.clear();
+    }
+    Ok((num_lines, num_bytes))
+}
+
+fn print_lines(
+    mut file: impl BufRead,
+    num_lines: &TakeValue,
+    total_lines: i64,
+) -> MyResult<()> {
+    if let Some(start) = get_start_index(num_lines, total_lines) {
+        let mut line_num = 0;
+        let mut buf = Vec::new();
+        loop {
+            let bytes_read = file.read_until(b'\n', &mut buf)?;
+            if bytes_read == 0 {
+                break;
+            }
+            if line_num >= start {
+                print!("{}", String::from_utf8_lossy(&buf));
+            }
+            line_num += 1;
+            buf.clear();
+        }
+    }
+    Ok(())
+}
+
+fn print_bytes<T>(
+    mut file: T,
+    num_bytes: &TakeValue,
+    total_bytes: i64,
+) -> MyResult<()>
+where
+    T: Read + Seek,
+{
+    if let Some(start) = get_start_index(num_bytes, total_bytes) {
+        file.seek(SeekFrom::Start(start))?;
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf)?;
+        if !buf.is_empty() {
+            print!("{}", String::from_utf8_lossy(&buf));
+        }
+    }
+    Ok(())
+}
+
+fn get_start_index(take_val: &TakeValue, total: i64) -> Option<u64> {
+    match take_val {
+        PlusZero => {
+            if total > 0 {
+                Some(0)
+            } else {
+                None
+            }
+        },
+        TakeNum(num) => {
+            if num == &0 || total == 0 || num > &total {
+                None
+            } else {
+                let start = if num < &0 { total + num } else { num - 1 };
+                Some(if start < 0 { 0 } else { start as u64 })
+            }
+        }
+    }
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    let num_files = config.files.len();
+    for (file_num, filename) in config.files.iter().enumerate() {
+        match File::open(filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(file) => {
+                if !config.quiet && num_files > 1 {
+                    println!(
+                        "{}==> {} <==",
+                        if file_num > 0 { "\n" } else { "" },
+                        filename
+                    );
+                }
+                let (total_lines, total_bytes) = count_lines_bytes(filename)?;
+                let file = BufReader::new(file);
+                if let Some(num_bytes) = &config.bytes {
+                    print_bytes(file, num_bytes, total_bytes)?;
+                } else {
+                    print_lines(file, &config.lines, total_lines)?;
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{
+        count_lines_bytes, get_start_index, parse_num, TakeValue::*,
+    };
+
+    #[test]
+    fn test_count_lines_bytes() {
+        let res = count_lines_bytes("tests/inputs/one.txt");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), (1, 24));
+
+        let res = count_lines_bytes("tests/inputs/ten.txt");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), (10, 49));
+    }
+
+    #[test]
+    fn test_get_start_index() {
+        // +0 from an empty file (0 lines/bytes) returns None
+        assert_eq!(get_start_index(&PlusZero, 0), None);
+
+        // +0 from a nonempty file returns an index that
+        // is one less than the number of lines/bytes
+        assert_eq!(get_start_index(&PlusZero, 1), Some(0));
+
+        // Taking 0 lines/bytes returns None
+        assert_eq!(get_start_index(&TakeNum(0), 1), None);
+
+        // Taking any lines/bytes from an empty file returns None
+        assert_eq!(get_start_index(&TakeNum(1), 0), None);
+
+        // Taking more lines/bytes than is available returns None
+        assert_eq!(get_start_index(&TakeNum(2), 1), None);
+
+        // When starting line/byte is less than total lines/bytes,
+        // return one less than starting number
+        assert_eq!(get_start_index(&TakeNum(1), 10), Some(0));
+        assert_eq!(get_start_index(&TakeNum(2), 10), Some(1));
+        assert_eq!(get_start_index(&TakeNum(3), 10), Some(2));
+
+        // When starting line/byte is negative and less than total,
+        // return total - start
+        assert_eq!(get_start_index(&TakeNum(-1), 10), Some(9));
+        assert_eq!(get_start_index(&TakeNum(-2), 10), Some(8));
+        assert_eq!(get_start_index(&TakeNum(-3), 10), Some(7));
+
+        // When the starting line/byte is negative and more than the total,
+        // return 0 to print the whole file
+        assert_eq!(get_start_index(&TakeNum(-20), 10), Some(0));
+    }
+
+    #[test]
+    fn test_parse_num() {
+        // All integers should be interpreted as negative numbers
+        let res = parse_num("3");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(-3));
+
+        // A leading "+" should result in a positive number
+        let res = parse_num("+3");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(3));
+
+        // An explicit "-" value should result in a negative number
+        let res = parse_num("-3");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(-3));
+
+        // Zero is zero
+        let res = parse_num("0");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(0));
+
+        // Plus zero is special
+        let res = parse_num("+0");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), PlusZero);
+
+        // Test boundaries
+        let res = parse_num(&i64::MAX.to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(i64::MIN + 1));
+
+        let res = parse_num(&(i64::MIN + 1).to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(i64::MIN + 1));
+
+        let res = parse_num(&format!("+{}", i64::MAX));
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(i64::MAX));
+
+        let res = parse_num(&i64::MIN.to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(i64::MIN));
+
+        // A floating-point value is invalid
+        let res = parse_num("3.14");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "3.14");
+
+        // Any non-integer string is invalid
+        let res = parse_num("foo");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "foo");
+    }
+}
---
+++ tailr/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    if let Err(err) = tailr::get_args().and_then(tailr::run) {
+        eprintln!("{}", err);
+        std::process::exit(1);
+    }
+}
